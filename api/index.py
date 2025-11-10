b3a63f6820d22ca3c279bb562b8c2c9e344a6f33
Deivide Maciel <31144383+deivomaciel@users.noreply.github.com>
2025-11-10 10:40:13 -0300

    Update index.py

--- api/index.py
+++ api/index.py
@@ -72,5 +72,4 @@ def getLLMResponse():
     return jsonify({'message': response})
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+    app.run(port=3001)
